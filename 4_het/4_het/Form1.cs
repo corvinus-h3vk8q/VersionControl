93ff5eb0b6dd69136feda73243fb7d7627a1eb1e
András Ferenc Simonyi <andras.simonyi@stud.uni-corvinus.hu>
2022-10-07 12:16:15 +0200

    excel object

--- 4_het/4_het/Form1.cs
+++ 4_het/4_het/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace _4_het
 {
@@ -21,7 +23,7 @@ namespace _4_het
         }
         private void LoadData()
         {
-            List<Flat> Flats = context.Flats.ToList()
+            List<Flat> Flats = context.Flats.ToList();
         }
     }
 }
