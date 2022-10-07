1165ac27e60f9d312ef14e8f707857cf09fd4781
András Ferenc Simonyi <andras.simonyi@stud.uni-corvinus.hu>
2022-10-07 12:50:39 +0200

    haladás2

--- 4_het/4_het/Form1.cs
+++ 4_het/4_het/Form1.cs
@@ -76,11 +76,42 @@ namespace _4_het
      "Ár (mFt)",
      "Négyzetméter ár (Ft/m2)"
             };
+
             for (int i = 0; i < headers.Length; i++)
             {
-                xlSheet.Cells[1, 1] = headers[0];
+                xlSheet.Cells[i + 1, 1] = headers[0];
             }
+
             object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                // ...
+                values[counter, 8] = "";
+                counter++;
+            }
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }
