2cfb11c02e8664b8aa7fd4661916812f3bf659db
András Ferenc Simonyi <andras.simonyi@stud.uni-corvinus.hu>
2022-12-02 12:44:01 +0100

    for_cycle_for_years

--- dec2/dec2/Form1.cs
+++ dec2/dec2/Form1.cs
@@ -17,14 +17,31 @@ namespace dec2
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+        Random rng = new Random(1235711);
 
         public Form1()
         {
             InitializeComponent();
-            /*  Population = GetPopulation(@"C:\Temp\nép.csv");
-              BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
-              DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
-            */
+            Population = GetPopulation(@"C:\Temp\nép.csv");
+            BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
+            DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
+
         }
         public List<Person> GetPopulation(string csvpath)
         {
@@ -69,7 +86,7 @@ namespace dec2
 
 
         }
-        public List<DeathProbability> deathProbabilities(string csvpath)
+        public List<DeathProbability> GetDeathProbabilities(string csvpath)
         {
             List<DeathProbability> deathprob = new List<DeathProbability>();
             using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
